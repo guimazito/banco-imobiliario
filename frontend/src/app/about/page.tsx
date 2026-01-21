f2aa264e484d4b24566be6ccd5832c249eeab9ab
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2026-01-21 19:22:05 -0400

    feat: add about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,14 +1,37 @@
+import {
+  Container,
+  Paper,
+  Typography,
+  Box
+} from "@mui/material";
 import { Navbar } from "../components/Navbar";
-import { PageUnderConstruction } from "../components/PageUnderConstruction";
 
 export default function AboutPage() {
   return (
     <>
-      <Navbar />
-      <PageUnderConstruction
-        title="Sobre"
-        description="A página Sobre está em desenvolvimento"
-      />
+      <Navbar inviteCode={undefined} gameId={undefined} />
+      <Container maxWidth="sm" sx={{ mt: 4 }}>
+        <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Sua Experiência, Nossa Solução
+          </Typography>
+          <Box mt={2}>
+            <Typography
+              variant="body1"
+              color="text.secondary"
+              sx={{ textAlign: "justify" }}
+            >
+              Este sistema web foi desenvolvido para facilitar o gerenciamento
+              das transações e do andamento de partidas de Banco Imobiliário.
+              Com ele, é possível registrar, acompanhar e visualizar todas as
+              movimentações financeiras dos jogadores, tornando a experiência do
+              jogo mais organizada, transparente e divertida. Ideal para grupos
+              que desejam focar na estratégia e diversão, sem se preocupar com o
+              controle manual do dinheiro.
+            </Typography>
+          </Box>
+        </Paper>
+      </Container>
     </>
   );
 }
